d2217c567605d4fcc05135c3e12dddb3586ef7df
Taylor Curry <currytay@gmail.com>
2022-01-22 13:06:54 +1100

    Add links

--- src/App.js
+++ src/App.js
@@ -9,23 +9,23 @@ const App = () => {
       </div>
       <div className='body-container'>
         <p className='body-text'>
-          Currently working on big projects at <a href='' target='_blank' className='link'>Accenture</a>
-          , but I recently shipped a few small ones of my own: <a href='' target='_blank' className='link'>Here Before A Million</a> and <a href='' target='_blank' className='link'>Peggy</a>.
+          Currently working on big projects at <a href='https://accenture.com/' target='_blank' className='link'>Accenture</a>
+          , but I recently shipped a few small ones of my own: <a href='https://herebeforeamillion.com/' target='_blank' className='link'>Here Before A Million</a> and <a href='https://peggy.world/' target='_blank' className='link'>Peggy</a>.
         </p>
       </div>
       <div className='link-container'>
         <ul className='link-list'>
           <li className='link-item'>
-            <a href='' target='_blank' className='link'>Email</a>
+            <a href='mailto:currytay@gmail.com?subject=Hellooo' target='_blank' className='link'>Email</a>
           </li>
           <li className='link-item'>
-            <a href='' target='_blank' className='link'>Twitter</a>
+            <a href='https://twitter.com/taylorcurry___' target='_blank' className='link'>Twitter</a>
           </li>
           <li className='link-item'>
-            <a href='' target='_blank' className='link'>LinkedIn</a>
+            <a href='https://linkedin.com/in/taylor-b-curry/' target='_blank' className='link'>LinkedIn</a>
           </li>
           <li className='link-item'>
-            <a href='' target='_blank' className='link'>GitHub</a>
+            <a href='https://github.com/currytay' target='_blank' className='link'>GitHub</a>
           </li>
         </ul>
       </div>
